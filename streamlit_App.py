8a50bc088a350ce39683f9015361ae3e642a660e
udayanagani <145590621+udayanagani@users.noreply.github.com>
2024-08-03 03:46:57 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd 
 #import plotly.express as px 
 #import seaborn as sns 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
-import pickle
+#import pickle
 import streamlit as st
 import time
 
