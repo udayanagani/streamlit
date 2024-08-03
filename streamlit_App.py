5feb4e933748d386e98d16137fbb7f9c1fcf5b71
udayanagani <145590621+udayanagani@users.noreply.github.com>
2024-08-03 21:12:34 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd 
-import plotly.express as px 
-import seaborn as sns 
-import matplotlib.pyplot as plt
+#import plotly.express as px 
+#import seaborn as sns 
+#import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-import streamlit as st
 import time
 
 app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
@@ -23,77 +22,11 @@ if app_mode=='Home':
     st.subheader("Below diagrams visualize the factots of distribution in the dataset.\n\n")
     df= pd.read_csv("heart.csv")
     
-
-    # Bar Plot for Outcome Counts
-    st.subheader('Distribution of Heart Attack Outcomes')
-    fig, ax = plt.subplots()
-    sns.countplot(x='output', data=df, ax=ax)
-    for patch in ax.patches:
-        current_width = patch.get_width()
-        diff = current_width - 0.3
-        # we change the bar width
-        patch.set_width(0.3)
-        # we recenter the bar
-        patch.set_x(patch.get_x() + diff * .5)
-    ax.set_title('Distribution of Heart Attack Outcomes')
-    ax.set_xlabel('Heart Attack Status')
-    ax.set_ylabel('Number of Individuals')
-    ax.set_xticklabels(['No Heart Attack risk', 'Heart Attack'])
-    st.pyplot(fig)
-    st.write("")
-    st.write("")
-    st.write("")
-
+    df = pd.DataFrame(
+        np.random.randn(10, 2),
+        columns=['x', 'y'])
+    st.line_chart(df)
     
-
-    age_counts = df['age'].value_counts().reset_index()
-    age_counts.columns = ['age', 'count']
-
-    #fig = px.pie(age_counts, values='count', names='age', title='Age Distribution of Individuals')
-    #st.plotly_chart(fig)
-    
-
-    st.subheader('Cholestoral Distribution')
-    fig, ax = plt.subplots()
-    sns.histplot(df['chol'], kde= True, bins=20, ax=ax)
-    ax.set_title('Cholestoral Ditribution of Individuals')
-    ax.set_xlabel('Cholestoral')
-    ax.set_ylabel('Frequency')
-    #ax.set_xticklabels(['No Heart Attack', 'Heart Attack'])
-    st.pyplot(fig)
-    st.write("")
-    st.write("")
-    st.write("")
-
-    
-    st.subheader('Resting Blood Pressure Distribution')
-    fig, ax = plt.subplots()
-    sns.histplot(df['trtbps'], kde= True, bins=20, ax=ax)
-    ax.set_title('Resting Blood Pressure Ditribution of Individuals')
-    ax.set_xlabel('Resting Blood Pressure Level')
-    ax.set_ylabel('Frequency')
-    st.pyplot(fig)
-    st.write("")
-    st.write("")
-    st.write("")
-
-    
-    st.subheader('Maximum Heart Rate Achieved Distribution')
-    st.write('Median Heart Rate(130) Achieved patients')
-    st.progress(37)
-
-    fig, ax = plt.subplots()
-    sns.histplot(df['thalachh'], kde= True, bins=20, ax=ax)
-    ax.set_title('Maximum Heart Rate Achieved Ditribution of Individuals')
-    ax.set_xlabel('Maximum Heart Rate Achieved')
-    ax.set_ylabel('Frequency')
-    st.pyplot(fig)
-    st.write("")
-    st.write("")
-    st.write("")
-
-    
-
 elif app_mode == 'Prediction':
 
 
